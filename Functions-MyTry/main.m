91aad52093f8a8c4a6ce295c76538f544ba0744c
fdesimini <fdesimini@users.noreply.github.com>
2015-04-08 16:22:39 -0400

    Initial Commit

---
+++ Functions-MyTry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Functions-MyTry
+//
+//  Created by Frank Desimini on 2015-04-08.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
