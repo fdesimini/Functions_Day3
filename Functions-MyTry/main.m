4a181908afe8c1f144bc0511a3b2cf500c69a7cc
fdesimini <fdesimini@users.noreply.github.com>
2015-04-08 16:42:03 -0400

    second commit

--- Functions-MyTry/main.m
+++ Functions-MyTry/main.m
@@ -8,15 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-int getRandomInteger(int minimum, int maximum) {
-    return arc4random_uniform((maximum - minimum)+1) + minimum;
+NSString *getRandomMake(NSArray *makes){
+    int maximum = (int)[makes count];
+    int randomIndex = arc4random_uniform(maximum);
+    return makes[randomIndex];
 }
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        int randomNumber = getRandomInteger(-10, 10);
-        NSLog(@"select a random number between 10 and -10: %d", randomNumber);
+        NSArray *makes = @[@"Honda", @"Ford", @"Nissan", @"Porsche"];
+        NSLog(@"Selected a %@", getRandomMake(makes));
+        
         
     }
     return 0;
